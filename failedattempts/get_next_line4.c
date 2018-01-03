9e0355df7cccfed4f6bb2bcf37ed39c71270ed90
Ted Tran <ttran@e1z3r8p23.42.us.org>
2018-01-02 16:06:32 -0800

    failed attempts

---
+++ failedattempts/get_next_line4.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line4.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ttran <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/12/31 15:14:35 by ttran             #+#    #+#             */
+/*   Updated: 2018/01/01 20:07:35 by ttran            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/*char	*ft_strdowns(const char *str)
+{
+	int		i;
+	char	*s2;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	i++;
+	s2 = malloc(sizeof(char) * i);
+	if (!s2)
+		return (ft_strnew(0));
+	i = 0;
+	while (str[i] != '\0')
+	{
+		s2[i] = str[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}*/
+
+void	gtfoline(char **holder, char **line)
+{
+	char	*tmp;
+	int		i;
+	int		n;
+
+	n = 0;
+	i = 0;
+	while ((*holder)[i] != '\n' && (*holder)[i] != '\0')
+		i++;
+	*line = malloc(sizeof(char) * (i + 1));
+	(*line)[i] = '\0';
+	i = 0;
+	while ((*holder)[i] != '\n' && (*holder)[i] != '\0')
+	{
+		(*line)[i] = (*holder)[i];
+		i++;
+	}
+	tmp = malloc(sizeof(char) * (ft_strlen(*holder) - i));
+	if (tmp)
+	{
+		i++;
+		while ((*holder)[i] != '\0')
+			tmp[n++] = (*holder)[i++];
+		tmp[n] = '\0';
+		free(*holder);
+		*holder = ft_strdup(tmp);
+		free(tmp);
+	}
+}
+
+int		spec_ops_the_line(char **holder, char **line)
+{
+	if ((*holder)[0] == '\0')
+	{
+		gtfoline(holder, line);
+		free(*holder);
+		return (0);
+	}
+	else
+		gtfoline(holder, line);
+	return (1);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	int			br;
+	char		buffer[BUFF_SIZE + 1];
+	static char	*arr[FD_LIMIT];
+	char		*tmp;
+
+	if (fd == -1 || read(fd, buffer, 0) < 0 || !line)
+		return (-1);
+	br = 1;
+	if (!(arr[fd]))
+		arr[fd] = ft_strnew(0);
+	while (ft_strchr(arr[fd], '\n') == NULL && br != 0)
+	{
+		br = read(fd, buffer, BUFF_SIZE);
+		buffer[br] = '\0';
+		tmp = ft_strjoin(arr[fd], buffer);
+		free(arr[fd]);
+		arr[fd] = ft_strdup(tmp);
+		free(tmp);
+	}
+	return (spec_ops_the_line(&arr[fd], line));
+}
